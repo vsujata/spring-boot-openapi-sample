00c8c2e85a2934f8a42ccd9410e962ea109a1e8b
Sujata Vyawahare <sujata.v25@gmail.com>
2023-03-14 20:16:58 +0530

    Initial commit

---
+++ spring-boot-openapi-sample/src/main/java/com/demo/springbootopenapisample/SpringBootOpenapiSampleApplication.java
@@ -0,0 +1,13 @@
+package com.demo.springbootopenapisample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootOpenapiSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootOpenapiSampleApplication.class, args);
+	}
+
+}
---
+++ spring-boot-openapi-sample/src/test/java/com/demo/springbootopenapisample/SpringBootOpenapiSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.springbootopenapisample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootOpenapiSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
